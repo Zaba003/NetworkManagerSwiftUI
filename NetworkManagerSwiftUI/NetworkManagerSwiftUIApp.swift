3e2c7c00e80e8d6c3ae10cacec13ce50ce37cecb
Kirill Zaborsky <avto.oil@gmail.com>
2021-08-29 20:08:32 +0700

    Initial Commit

---
+++ NetworkManagerSwiftUI/NetworkManagerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManagerSwiftUIApp.swift
+//  NetworkManagerSwiftUI
+//
+//  Created by Кирилл Заборский on 29.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct NetworkManagerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
